a1ab6a12152886b9c61a88c45c5055cbe5a68690
root <suppermick@gmail.com>
2020-06-11 04:50:10 +0000

    animate star rating

--- app.js
+++ app.js
@@ -14,13 +14,14 @@ var express    = require("express"),
 var commentRoutes 		= require("./routes/comments"),
 	campgroundRoutes 	= require("./routes/campgrounds"),
 	indexRoutes 		= require("./routes/index");
+var path = require('path');
 
-//seedDB();
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect("mongodb://localhost:27017/yelp_camp_v2", {useNewUrlParser: true});
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
