14ba8bfda1988912f77b42ab475a35f77926d1e5
root <suppermick@gmail.com>
2020-06-24 09:10:34 +0000

    add password match validation

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,25 @@
 
 $(function(){
 	console.log("ready");
+	$("#pwConfirm").keyup(validate);
 
 });
+
+function validate() {
+  var password1 = $("#pw").val();
+  var password2 = $("#pwConfirm").val();
+
+    
+ 
+    if(password1 == password2) {
+		$("#valid").text("");
+		$("#reg-btn").removeClass("disabled");
+		$("#reg-btn").prop('disabled', false);
+    }
+    else {
+        $("#valid").text("Passwords must match!");
+		$("#reg-btn").addClass("disabled");
+		$("#reg-btn").prop('disabled', true);
+    }
+    
+}
