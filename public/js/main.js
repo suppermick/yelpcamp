d517f7c2dd29ecb6accd1fbaca65bb44778c6d71
root <suppermick@gmail.com>
2020-06-26 04:20:40 +0000

    style page transitions

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,47 @@
 
 $(function(){
-	console.log("ready");
+	
+	$(".body-content").animate({opacity:1});	
+	
 	$("#pwConfirm").keyup(validate);
 	
 	$.each($('.before-animation'), function(i, el){
-    setTimeout(function(){
-		$(el).addClass('slideFromRight')
-    },200 + ( i * 200 ));
+		setTimeout(function(){
+			$(el).addClass('slideFromRight')
+		},200 + ( i * 200 ));
+	
+	});
+	
+	$("a").click(function(event){
+		
+		event.preventDefault();
+		linkLocation = this.href;
+		
+		if ( $(this).attr('target') == '_blank' ){
+			
+			return;
+		
+		} else if ( $(this).hasClass("camp-link")) {
+			
+			$.each($('.before-animation'), function(i, el){
+				setTimeout(function(){
+					$(el).addClass('slideToLeft')
+				},100 + ( i * 50 ));
+				
+			});
+			$(".body-content").delay(650).fadeOut(100, redirectPage);
+			
+		} else {
+				   
+			$(".body-content").fadeOut(100, redirectPage);
+		}
+	});
+	
 });
 
-});
+function redirectPage() {
+    window.location = linkLocation;
+}
 
 function validate() {
   var password1 = $("#pw").val();
